679a2693548ae02ab99ab6d4afd60e814555edcb
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-05-31 14:17:48 +0900

    TS / Index Type

---
+++ 9-advanced/9-2-index.ts
@@ -0,0 +1,33 @@
+{
+  const obj = {
+    name: 'kim',
+  }
+  obj.name; // kim
+  obj['name']; // kim
+
+  type Animal = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+  };
+
+  type Name = Animal['name'] // string
+  const text: Name = 'only string';
+
+  type Gender = Animal['gender']; // 'male' | 'female'
+
+  type Keys = keyof Animal; // 'name' | 'age' | 'gender'
+  const key: Keys = 'gender';
+
+  type Person = {
+    name: string;
+    gender: Animal['gender'];
+  };
+
+  const person: Person = {
+    name: 'kim',
+    gender: 'male',
+  }
+
+
+}
