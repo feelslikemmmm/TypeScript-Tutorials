a8231c9d5abd7087c446f0535071aeb80e6ee44d
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-06-21 16:16:44 +0900

    TS / sourceMap debugger

--- 11-config/src/main.ts
+++ 11-config/src/main.ts
@@ -1,1 +1,11 @@
-console.log('hahaha!');
+'use strict';
+class Car {
+  engine = 0;
+  move() {
+    const engine = this.engine + 1;
+    console.log(engine);
+  }
+}
+
+const car = new Car();
+car.move();
